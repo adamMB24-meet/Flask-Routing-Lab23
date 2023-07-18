4cd37332812ac7d148289a671832829fe6cc8554
adamMB24-meet <139688738+adamMB24-meet@users.noreply.github.com>
2023-07-18 16:26:19 +0300

    CS Y2 Flask Lab
    First Flask Lab, abut EBay! CS Y2.

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,8 +8,17 @@ app = Flask(  # Create a flask app
 )
 
 # Your code should be below
+@app.route('/')
+def home_html():
+    return render_template("home.html")
 
+@app.route('/product_1')
+def product_html():
+    return render_template ("product.html")
 
+@app.route('/product_2')
+def cart_html():
+    return render_template("cart.html")
 
 
 # Your code should be above
